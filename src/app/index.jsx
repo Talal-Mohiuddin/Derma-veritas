b6e58fdda61e37621bb01be7dc0801c0ffe30db6
Talal Mohiuddin <hafiztalal8722@gmail.com>
2025-08-28 14:54:06 +0500

    feat: Allow admin page access in login page check

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 
 const ChildLayout = ({ children }) => {
   const pathname = usePathname();
-  const isLoginPage = pathname === "/login";
+  const isLoginPage = pathname === "/login" || pathname.includes("/admin");
 
   return (
     <AuthProvider>
