de57efe010c94c13971a867b2a4d913b2b1726aa
Talal Mohiuddin <hafiztalal8722@gmail.com>
2025-09-03 16:59:29 +0500

    fix: Wrap logo in a link to improve navigation on the LoginPage

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,13 +10,15 @@ export default function LoginPage() {
     <div className="flex flex-col w-full items-center min-h-screen gap-8 justify-center bg-gradient-to-b from-gray-50 to-white">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-xl shadow-xl border border-gray-100">
         <div className="text-center">
-          <Image
-            src="/logo.svg"
-            alt="Logo"
-            width={60}
-            height={60}
-            className="mx-auto"
-          />
+          <Link href="/" >
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={60}
+              height={60}
+              className="mx-auto"
+            />
+          </Link>
           <h2 className="mt-6 text-3xl font-light text-gray-900">
             Welcome to DermaVeritas
           </h2>
